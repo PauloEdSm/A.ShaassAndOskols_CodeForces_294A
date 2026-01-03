bc173e386c4a490eaf15cf9541968ecc3280cf9f
paulodev <pauloeduardotroll9646@gmail.com>
2026-01-03 15:42:32 -0300

    Beckup

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int numberOfWires = scanner.nextInt();
+        int [] wires = new int[numberOfWires];
+        int shotPosition;
+        for (int j = 0; j < numberOfWires ; j++) {
+            wires[j] = scanner.nextInt();
+        }
+        int shots = scanner.nextInt();
+        while (shots-->0) {
+
+        }
     }
 }
