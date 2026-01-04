08243ea23b37d8cb101ae6d4192869128669d26d
paulodev <pauloeduardotroll9646@gmail.com>
2026-01-04 19:37:13 -0300

    Final Version

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,23 @@ public class Main {
         int numberOfWires = scanner.nextInt();
         int [] wires = new int[numberOfWires];
         int shotPosition;
+        int targetWire;
         for (int j = 0; j < numberOfWires ; j++) {
             wires[j] = scanner.nextInt();
         }
         int shots = scanner.nextInt();
         while (shots-->0) {
-
+            targetWire = scanner.nextInt()-1;
+            shotPosition = scanner.nextInt();
+            int birds = wires[targetWire];
+            if (targetWire > 0) {
+                wires[targetWire - 1] += shotPosition - 1;
+            }
+            if (targetWire < numberOfWires - 1) {
+                wires[targetWire + 1] += wires[targetWire] - shotPosition;
+            }
+                wires[targetWire] = 0;
+            }
+        Arrays.stream(wires).forEach(System.out::println);
         }
     }
-}
